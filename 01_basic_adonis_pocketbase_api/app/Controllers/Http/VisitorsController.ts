257afca3abb2f84f0672a4b7c1d963e9fc6abc84
florentazd <florent4real@yahoo.com>
2022-12-01 14:36:07 +0100

    successfully create the login controller of 01

--- 01_basic_adonis_pocketbase_api/app/Controllers/Http/VisitorsController.ts
+++ 01_basic_adonis_pocketbase_api/app/Controllers/Http/VisitorsController.ts
@@ -12,7 +12,32 @@ export default class VisitorsController {
      * ### Retour StatusCode en fonction du resultat de l'authentification
      */
     public async login(ctx: HttpContextContract) {
-        console.log("Hello" + ctx.request.url())
+        const { identity, password } = ctx.request.body()
+
+        if (!identity || !password)
+            ctx.response.status(400).send({
+                "error": "Au moins un des champs est vide"
+            })
+        else {
+            // Creation du payload
+            const payload = {
+                identity: identity,
+                password: password
+            }
+
+            // Creation du client pocketbase pour l'envoie de la requete
+            const pb_client = new PocketBase("http://127.0.0.1:8090")
+
+            // Envoie du payload vers la base de données
+            try {
+                const authData = await pb_client.collection("users").authWithPassword(payload.identity, payload.password)
+
+                if (authData)
+                    ctx.response.status(200).send({ authData })
+            } catch {
+                ctx.response.status(401).send("Invalid credentials")
+            }
+        }
     }
 
     /**
